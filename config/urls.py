30977e4aa98f4633ca26b6dd056d36b2ecc5efa0
Frank Garzon <fsebastian.garzong@gmail.com>
2026-02-26 16:04:39 -0500

    redactor: Se modifica odifica el ViewSet y se agregan token ene el archivo api.py

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)# El archivo urls.py es el archivo de configuración de URL para el proyecto Django. Define las rutas que se asignan a las vistas de la aplicación. En este caso, se incluyen las rutas para el panel de administración de Django, las rutas para la aplicación 'usuarios' y las rutas para el inicio de sesión y la actualización de tokens utilizando la biblioteca Simple JWT. Cada ruta se asigna a una vista específica que se encargará de manejar las solicitudes HTTP correspondientes.
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('usuarios.urls')),
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # La ruta 'api/login/' se asigna a la vista TokenObtainPairView, que es proporcionada por la biblioteca Simple JWT. Esta vista se encarga de manejar las solicitudes de inicio de sesión y generar un par de tokens (access y refresh) para el usuario autenticado. El nombre 'token_obtain_pair' se utiliza para referenciar esta ruta en otras partes del código, como en las pruebas o en la documentación de la API.
+    path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # La ruta 'api/refresh/' se asigna a la vista TokenRefreshView, que también es proporcionada por la biblioteca Simple JWT. Esta vista se encarga de manejar las solicitudes de actualización de tokens, lo que permite a los usuarios obtener un nuevo token de acceso utilizando un token de actualización válido. El nombre 'token_refresh' se utiliza para referenciar esta ruta en otras partes del código, como en las pruebas o en la documentación de la API.
 ]
--- usuarios/api.py
+++ usuarios/api.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Usuario
 from .serializers import UsuarioSerializer
 
@@ -6,3 +7,4 @@ from .serializers import UsuarioSerializer
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all() # Define el conjunto de datos que se utilizará para las operaciones CRUD, en este caso, todos los objetos de Usuario.
     serializer_class = UsuarioSerializer # Especifica el serializer que se utilizará para convertir los objetos de Usuario a formatos como JSON o XML y viceversa, en este caso, el UsuarioSerializer.
+    permission_classes = [IsAuthenticated] # Establece que solo los usuarios autenticados pueden acceder a esta vista, lo que significa que se requiere autenticación para realizar cualquier operación en el conjunto de vistas de Usuario.

58e836c5afbe51b0797da964b53d7440c25f2be1
Frank Garzon <fsebastian.garzong@gmail.com>
2026-03-03 14:11:57 -0500

    fix: correeccion de la pliamentacion del filter

--- compras/views.py
+++ compras/views.py
@@ -3,18 +3,12 @@ from rest_framework import viewsets
 from compras.models import Compra
 from compras.serializers import CompraSerializer
 from .permissions import EsAdminOCompras
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import OrderingFilter
 
 
-class CompraViewSet(viewsets.ModelViewSet):
+class CompraView(viewsets.ModelViewSet):
     queryset = Compra.objects.all()
     serializer_class = CompraSerializer
     permission_classes = [EsAdminOCompras]
-    
-    # Agrega filtros y ordenamiento
-    filterset_fields = ['proveedor', 'fecha']
-    ordering_fields = ['fecha']
 
     def get_queryset(self):
      user = self.request.user

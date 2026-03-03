eed38389a182bcc489353d1bf90526c07d12d09b
Frank Garzon <fsebastian.garzong@gmail.com>
2026-03-03 15:57:05 -0500

    test: pruebas

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 import { routes } from './app.routes';
 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  imports: [RouterOutlet]
+})
+export class AppComponent {}
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),

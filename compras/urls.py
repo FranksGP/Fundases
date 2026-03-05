a5a93e52dea10d3ee1c6db77c5f7de7961524278
Frank Garzon <fsebastian.garzong@gmail.com>
2026-03-05 11:53:01 -0500

    refactor: Se reorganiza el back-end y semueve todo a la carpeta 'backend'.

--- compras/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .api import CompraViewSet
-from .views import CompraView
-
-router = DefaultRouter()
-router.register(r'compras', CompraViewSet, CompraView)
-
-urlpatterns = router.urls
--- proveedores/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .api import ProveedorViewSet
-
-router = DefaultRouter()
-router.register(r'proveedores', ProveedorViewSet)
-
-urlpatterns = router.urls
